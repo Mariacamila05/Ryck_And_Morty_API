d21da71a878cd9dce8e2d717b4cb3482c56f742e
mariacamila <mendezcamila935@gmail.com>
2023-06-18 22:18:00 -0500

    Actividad 4

---
+++ Api Rick y Morty/index.js
@@ -0,0 +1,29 @@
+function getcharacters(done){
+    const results = fetch("https://rickandmortyapi.com/api/character");
+
+    results
+    .then(Response => Response.json())
+    .then(data =>{
+        done(data)
+    });
+}
+
+getcharacters(data => {
+    data.results.forEach(personaje => {
+        const article = document.createRange().createContextualFragment(/*html*/`
+        <article>
+            <div class="image-container">
+                <img src="${personaje.image}" alt="Personaje">
+            </div>
+
+            <h2>${personaje.name}</h2>
+            <span>${personaje.status}</span>
+        </article>
+        
+        `);
+
+        const main = document.querySelector("main");
+        main.append(article);
+
+    });
+});
